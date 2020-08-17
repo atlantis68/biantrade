489b11dcaf1d34e8dda0f65be0127b0ed6074687
duheng <heng.du@dmall.com>
2020-08-17 10:50:22 +0800

    fix bugs

--- SSM_web/src/main/java/cn/itcast/controller/OrderController.java
+++ SSM_web/src/main/java/cn/itcast/controller/OrderController.java
@@ -190,10 +190,13 @@ public class OrderController {
 			int level = 1;
 			String[] roles = user.getRole().split(",");
 			for(String role : roles) {
-				int atomic = Integer.parseInt(role);
-				if(atomic < 6 && atomic > level) {
-					level = atomic;
-				}
+    			if(StringUtils.isNotEmpty(role)) {
+    				int atomic = Integer.parseInt(role);
+    				if(atomic < 6 && atomic > level) {
+    					level = atomic;
+    				}
+    				
+    			}
 			}
 			List<Plan> plans = orderService.findPlanByTime(startTime, level);  
         	result.put(TransactionConstants.SYSTEM_STATUS, TransactionConstants.SYSTEM_STATUS_OK);
